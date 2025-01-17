df8f0219c69db0fb77cf377c8936883e0f011cb4
Bento-Box <bento.robotics@gmail.com>
2025-01-17 15:07:24 +0100

    update parameters, add tf for laser

--- container/launch-content/bento-box.launch.py
+++ container/launch-content/bento-box.launch.py
@@ -26,6 +26,7 @@ def generate_launch_description():
         executable='bento_drive_node',
         name='bento_drive_node',
         parameters=[ PathJoinSubstitution([ '/', 'launch-content', 'parameters', 'bento-box.yaml' ]) ],
+        remappings=[('odom', '/odom' ), ('pose', '/pose')],
         output='screen',
 	emulate_tty=True,
     )
@@ -72,6 +73,15 @@ def generate_launch_description():
         }.items(),
     )
 
+    tf2_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_link_to_base_tof',
+        arguments=["--x", "0.0","--y", "0.0","--z", "0.1","--roll", "0.0","--pitch", "0.0","--yaw", "0.0",
+        #TODO add values
+        "--frame-id", "base_link","--child-frame-id", "laser"],
+        output="screen"
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -86,6 +96,8 @@ def generate_launch_description():
             camera_ros_1,
             camera_ros_2,
             bento_drive,
-            lidar,
-        ])
+            #lidar,
+        ]),
+        lidar,
+        tf2_node
     ])
